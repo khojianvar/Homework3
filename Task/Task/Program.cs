064697664da5865cbb6883c5f62fba610a67e25a
khojianvar <khojianvarabdumukhtorov@gmail.com>
2023-03-27 12:54:20 +0500

    Task1 finished

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter the number: ");
+            int num = int.Parse(Console.ReadLine());
+            string new_num = "";
+            for (; num != 0;)
+            {
+                if (num % 2 != 0)
+                {
+                    num = num / 2;
+                    new_num = new_num + "1";
+                }
+                if (num % 2 == 0)
+                {
+                    num = num / 2;
+                    new_num = new_num + "0";
+                }
+            }
+            for (int i = new_num.Length - 1; i >= 0; i--)
+            {
+                Console.Write(new_num[i]);
+            }
+            Console.WriteLine();
+            Main(args);
         }
     }
 }
