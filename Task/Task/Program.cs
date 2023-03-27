905de63aef10ccc61b80698ff4d39651c06ef31b
khojianvar <khojianvarabdumukhtorov@gmail.com>
2023-03-27 13:02:28 +0500

    Task3 is finished

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter the firstnumber: ");
+            int firstnumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter the secondnumber: ");
+            int secondnumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter the thirdnumber: ");
+            int thirdnumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter the fourthnumber: ");
+            int fourthnumber = int.Parse(Console.ReadLine());
+            int maxnumber = firstnumber;
+            if (maxnumber < secondnumber)
+            {
+                maxnumber = secondnumber;
+            }
+            if (maxnumber < thirdnumber)
+            {
+                maxnumber = thirdnumber;
+            }
+            if (maxnumber < fourthnumber)
+            {
+                maxnumber = fourthnumber;
+            }
+            int minnumber = firstnumber;
+            if (minnumber > secondnumber)
+            {
+                minnumber = secondnumber;
+            }
+            if (minnumber > thirdnumber)
+            {
+                minnumber = thirdnumber;
+            }
+            if (minnumber > fourthnumber)
+            {
+                minnumber = fourthnumber;
+            }
+            int res = maxnumber - minnumber;
+            Console.WriteLine($"The max number: {maxnumber}");
+            Console.WriteLine($"The min number: {minnumber}");
+            Console.WriteLine($"Difference between the largest and the smallest of them: {res}");
         }
     }
 }
