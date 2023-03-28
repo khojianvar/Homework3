ef19a4d1ea72c27b5c31c4243616231e50eb1e3a
khojianvar <khojianvarabdumukhtorov@gmail.com>
2023-03-28 19:31:38 +0500

    task8

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter starting range: ");
+            int srange = int.Parse(Console.ReadLine());
+            Console.Write("Enter ending range: ");
+            int erange = int.Parse(Console.ReadLine());
+            Console.Write($"Result: The Perfect numbers within {srange} and {erange} are:");
+            for (int i = srange; i <= erange; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum += j;
+                    }
+                }
+                if (sum == i)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
