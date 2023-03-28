f00438c145bddbaab0e66a0a410d1d144c72ffa5
khojianvar <khojianvarabdumukhtorov@gmail.com>
2023-03-28 19:34:52 +0500

    Task9

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter the number: ");
+            int number = int.Parse(Console.ReadLine());
+            int nDivision = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    nDivision++;
+                }
+            }
+            if (nDivision == 1)
+            {
+                Console.WriteLine($"{number}-isnt prime and isnt not prime!");
+            }
+            else if (nDivision == 2)
+            {
+                Console.WriteLine($"{number}-is a prime number");
+            }
+            else
+            {
+                Console.WriteLine($"{number}-is not a prime number");
+            }
         }
     }
 }
