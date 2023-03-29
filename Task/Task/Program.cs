a8a35287d154bc697e2ac2eec36868eca64bd960
khojianvar <khojianvarabdumukhtorov@gmail.com>
2023-03-29 13:11:57 +0500

    Improved the code

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -12,36 +12,12 @@
             int thirdnumber = int.Parse(Console.ReadLine());
             Console.Write("Enter the fourthnumber: ");
             int fourthnumber = int.Parse(Console.ReadLine());
-            int maxnumber = firstnumber;
-            if (maxnumber < secondnumber)
-            {
-                maxnumber = secondnumber;
-            }
-            if (maxnumber < thirdnumber)
-            {
-                maxnumber = thirdnumber;
-            }
-            if (maxnumber < fourthnumber)
-            {
-                maxnumber = fourthnumber;
-            }
-            int minnumber = firstnumber;
-            if (minnumber > secondnumber)
-            {
-                minnumber = secondnumber;
-            }
-            if (minnumber > thirdnumber)
-            {
-                minnumber = thirdnumber;
-            }
-            if (minnumber > fourthnumber)
-            {
-                minnumber = fourthnumber;
-            }
-            int res = maxnumber - minnumber;
-            Console.WriteLine($"The max number: {maxnumber}");
-            Console.WriteLine($"The min number: {minnumber}");
-            Console.WriteLine($"Difference between the largest and the smallest of them: {res}");
+            
+            int MaxNum = Math.Max(Math.Max(firstnumber, secondnumber), Math.Max(thirdnumber, fourthnumber));
+            int MinNum = Math.Min(Math.Min(firstnumber, secondnumber), Math.Min(thirdnumber, fourthnumber));
+            Console.WriteLine($"The max number: {MaxNum}");
+            Console.WriteLine($"The min number: {MinNum}");
+            Console.WriteLine($"Difference between the largest and the smallest of them: {MaxNum - MinNum}");
         }
     }
 }
