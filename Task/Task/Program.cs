52adeeee2a7d5e49bfb50ea0701e720ff18b8a1f
khojianvar <khojianvarabdumukhtorov@gmail.com>
2023-04-03 23:13:00 +0500

    task5

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter the number: ");
+            int previusNumber = int.Parse(Console.ReadLine());
+            int sequence = 1;
+            int longSequence = 0;
+            int numberOfLongSequence = previusNumber;
+            for (;true;)
+            {
+                Console.Write("Enter the number: ");
+                int currentNumber = int.Parse(Console.ReadLine());
+                if(previusNumber == currentNumber)
+                {
+                    sequence++;
+                }
+                else if (currentNumber == 0)
+                {
+                    break;
+                }
+                else 
+                {
+                    if(longSequence<sequence)
+                    {
+                        longSequence = sequence;
+                        numberOfLongSequence = previusNumber;
+                    }
+                    sequence = 1;
+                }
+                previusNumber = currentNumber;
+            }
+            Console.WriteLine($"Longest sequence: {longSequence} times number {numberOfLongSequence}");
         }
     }
 }
